468b694ed49cc2607a32dd22c48f8d1e4f0c728f
Desd.py <desolatedependency@gmail.com>
2025-04-17 20:42:43 +0200

    Update disk-configuration.nix

--- disk-configuration.nix
+++ disk-configuration.nix
@@ -4,7 +4,7 @@
         disk = { # ---------------------------------------------------------> all disks installed in the system like SSDs, HHDs, M.2s etc.
             main = { # -----------------------------------------------------> disk installed in a NVM Express port (name freely selectable)
                 type = "disk"; # -------------------------------------------> marks "nvme0n1" as a disk
-                device = "/dev/<disk-name>"; # -----------------------------> path to the defined disk
+                device = "/dev/<name-of-drive>"; # -----------------------------> path to the defined disk
                 content = { # ----------------------------------------------> contains everything that should happen on this disk
                     type = "gpt"; # ----------------------------------------> partitioning style (GUID Partition Table [gpt] or Master Boot Record [mbr])
                     partitions = { # ---------------------------------------> partitions created on this disk
